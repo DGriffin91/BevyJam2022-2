8effcd4def8136575e46b28fbe10b9a1e79e74fd
Griffin <33357138+DGriffin91@users.noreply.github.com>
2022-08-19 12:00:33 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("icon.png"),
+        ..Default::default()
+    });
+}
